e0eb898f2a7a00dec472a24b477eff14f79f391c
2018wzh <wzh20070720@outlook.com>
2026-01-02 18:26:19 +0800

    fix building

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Build the project
+cargo build --target riscv64gc-unknown-none-elf
+
+# Create build directory
+mkdir -p build
+
+# Convert ELF to binary
+riscv64-unknown-elf-objcopy -O binary ../../target/riscv64gc-unknown-none-elf/debug/factotum build/factotum.bin
