a6b09031f3c62f52cac334e29be4eaa9d47112a3
2018wzh <wzh20070720@outlook.com>
2026-01-04 02:51:39 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 set -e
 
+MODE="debug"
+CARGO_FLAGS=""
+
+for arg in "\$@"; do
+  if [ "\$arg" = "--release" ]; then
+    MODE="release"
+    CARGO_FLAGS="--release"
+  fi
+done
+
 # Build the project
-cargo build --target riscv64gc-unknown-none-elf
+cargo build --target riscv64gc-unknown-none-elf $CARGO_FLAGS
 
 # Create build directory
 mkdir -p build
 
-# Convert ELF to binary
-# riscv64-unknown-elf-objcopy -O binary ${CARGO_MANIFEST_DIR}/../target/riscv64gc-unknown-none-elf/debug/factotum build/factotum.bin
-cp ${CARGO_MANIFEST_DIR}/../target/riscv64gc-unknown-none-elf/debug/factotum build/factotum.elf
+# Copy ELF to build directory
+cp ${CARGO_MANIFEST_DIR}/../target/riscv64gc-unknown-none-elf/$MODE/factotum build/factotum.elf
